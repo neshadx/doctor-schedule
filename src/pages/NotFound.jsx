51384b7839bd1307bd8cd27180c1484ea9a47308
neshadx <neshadimagify@gmail.com>
2025-04-24 22:16:26 +0600

    added some code

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,11 +6,15 @@ const NotFound = () => {
     <>
     <Navbar/>
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-50 text-center px-4">
-      <h1 className="text-6xl font-bold text-blue-600 mb-4">404</h1>
+      <h1 className="text-6xl font-bold text-blue-700 mb-4">404</h1>
+      {/* <img className=" h-72  shadow-emerald-800" src="https://i.ibb.co.com/0R0mBmZg/6342464.jpg" alt="" /> */}
       <h2 className="text-2xl font-semibold mb-2">Oops! Page not found.</h2>
       <p className="text-gray-600 mb-6">
         The page you’re looking for doesn’t exist or has been moved.
       </p>
+
+
+      
       <Link
         to="/"
         className="px-6 py-3 bg-blue-600 text-white rounded-full hover:bg-blue-700 transition"
